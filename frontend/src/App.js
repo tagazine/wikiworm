46ed5cdb8d16dbb30af3348c2ec340182a978591
Your Namegit config --global user.name Your <you@example.com>
2022-09-26 21:10:08 -0400

    Deploying

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
   const [getMessage, setGetMessage] = useState({})
 
   useEffect(()=>{
-    axios.get('http://localhost:5000/flask/hello').then(response => {
+    axios.get('http://wikiworm.heroku.app.comm/flask/hello').then(response => {
       console.log("SUCCESS", response)
       setGetMessage(response)
     }).catch(error => {
